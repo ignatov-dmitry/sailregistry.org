8d3edbc9575ab79d7384c34592442346fa3319d7
ignatov-dmitry <dzmitry.ihnatau@instinctools.com>
2022-06-08 15:50:59 +0300

    fix admin certificate

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -5,5 +5,5 @@ require('admin-lte/dist/js/adminlte')
 window.bsCustomFileInput = require('admin-lte/plugins/bs-custom-file-input/bs-custom-file-input');
 
 $('#certificate_type_parent_id').selectize({
-    placeholder: 'Школы',
+    placeholder: 'Родительский сертификат',
 });
